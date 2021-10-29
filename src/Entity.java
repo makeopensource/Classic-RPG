39085c8f69ad8b2b51e234570b2a57d18bd14d7f
Trevor Schupbach <trevorschupbach@gmail.com>
2021-10-29 16:02:12 -0400

    Updated Formatting
    Adjusted the format to fit with what is present in Tile.java. It's gross but whatever. Also fixed Tile.java since I renamed Entities.

--- src/Entity.java
+++ src/Entity.java
@@ -1,26 +1,23 @@
 public class Entity {
+    
     private Position position;
 
-    //Constructor for using reference (equipped/held items)
-    public Entity(Position position)
-    {   
-        this.position = position;
-    }
-
-    //Constructor for just being on the map
-    public Entity(Region region, int x, int y)
-    {
-        this.position = new Position(region, x, y);
-    }
-
-    public Position getPosition()
-    {
+    public Position getPosition(){
         return position;
     }
 
-    public void setPosition(Position position)
-    {
+    public void setPosition(Position position){
         //Important that the address doesn't change but the values do
         this.position.set(position);
+    }
+
+
+    //Constructor for using reference (equipped/held items)
+    public Entity(Position position){   
+        this.position = position;
+    }
+
+    public Entity(Region region, int x, int y){
+        this.position = new Position(region, x, y);
     }
 }
--- src/Position.java
+++ src/Position.java
@@ -1,64 +1,57 @@
 public class Position {
-   private Region region;
-   private int x, y;
+    
+    private Region region;
+    private int x, y;
 
-   public Position(Region region, int x, int y)
-   {
-       this.region = region;
-       this.x = x;
-       this.y = y;
-   }
-
-   @Override
-   public boolean equals(Object o)
-   {
-       if (this == o) return true;
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
        
-       if (!(o instanceof Position)) return false;
+        if (!(o instanceof Position)) return false;
+
+        Position other = (Position)(o);
+
+        if (this.region != other.region) return false;
+        if (this.x != other.x) return false;
+        if (this.y != other.y) return false;
        
-       Position other = (Position)(o);
+        return true;
+    }
 
-       if (this.region != other.region) return false;
-       if (this.x != other.x) return false;
-       if (this.y != other.y) return false;
-       
-       return true;
-   }
+    public void set(Position position){
+        this.region = position.region;
+        this.x = position.x;
+        this.y = position.y;
+    }
 
-   public void set(Position position)
-   {
-       this.region = position.region;
-       this.x = position.x;
-       this.y = position.y;
-   }
+    public void setRegion(Region region){
+        this.region = region;
+    }
 
-   public Region getRegion()
-   {
-       return region;
-   }
+    public Region getRegion(){
+        return region;
+    }
 
-   public void setRegion(Region region)
-   {
-       this.region = region;
-   }
+    public void setX(int x){
+        this.x = x;
+    }
 
-   public int getX()
-   {
-       return x;
-   }
+    public int getX(){
+        return x;
+    }
 
-   public void setX(int x)
-   {
-       this.x = x;
-   }
+    public void setY(int y){
+        this.y = y;
+    }
 
-   public int getY()
-   {
-       return y;
-   }
+    public int getY(){
+        return y;
+    }
 
-   public void setY(int y)
-   {
-       this.y = y;
-   }
+
+    public Position(Region region, int x, int y){
+        this.region = region;
+        this.x = x;
+        this.y = y;
+    }
 }
