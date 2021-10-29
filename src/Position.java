398591c45e41ad0477edd6c8b86402d84ae42056
Trevor Schupbach <trevorschupbach@gmail.com>
2021-10-29 15:54:14 -0400

    Implement Entity Class
    Renamed the class from Entites to Entity since the design document had conflicting naming and Entity made more sense in this case. Added a Position class that stores a region, x, and y. This is mainly important so that Entities can hold references to other Entities Positions not just have their own unique location. Added the necessary setters and getters and constructors. Overrode the Position equality operator to support actual comparisons of positions.

---
+++ src/Position.java
@@ -0,0 +1,64 @@
+public class Position {
+   private Region region;
+   private int x, y;
+
+   public Position(Region region, int x, int y)
+   {
+       this.region = region;
+       this.x = x;
+       this.y = y;
+   }
+
+   @Override
+   public boolean equals(Object o)
+   {
+       if (this == o) return true;
+       
+       if (!(o instanceof Position)) return false;
+       
+       Position other = (Position)(o);
+
+       if (this.region != other.region) return false;
+       if (this.x != other.x) return false;
+       if (this.y != other.y) return false;
+       
+       return true;
+   }
+
+   public void set(Position position)
+   {
+       this.region = position.region;
+       this.x = position.x;
+       this.y = position.y;
+   }
+
+   public Region getRegion()
+   {
+       return region;
+   }
+
+   public void setRegion(Region region)
+   {
+       this.region = region;
+   }
+
+   public int getX()
+   {
+       return x;
+   }
+
+   public void setX(int x)
+   {
+       this.x = x;
+   }
+
+   public int getY()
+   {
+       return y;
+   }
+
+   public void setY(int y)
+   {
+       this.y = y;
+   }
+}
