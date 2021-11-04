54159447509791a95a95e230900801b7176c934e
adam <adam38999@gmail.com>
2021-11-04 18:06:43 -0400

    Added Position Tests

---
+++ src/Tests/BaseClassesTests/PositionTest.java
@@ -0,0 +1,90 @@
+package Tests.BaseClassesTests;
+
+import org.junit.jupiter.api.Test;
+import BaseClasses.Position;
+import BaseClasses.Region;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionTest {
+    Region testregion1 = new Region();
+    Region testregion2 = new Region();
+
+    @Test
+    void testEquals() {
+        Position tp1 = new Position(testregion1, 1 ,1);
+        Position tp2 = new Position(testregion1, 1 ,1);
+        assertEquals(tp1, tp2);
+        Position tp3 = new Position(testregion1, 1 ,1);
+        Position tp4 = new Position(testregion1, 1 ,2);
+        assertNotEquals(tp3, tp4);
+        Position tp5 = new Position(testregion1, 1 ,1);
+        Position tp6 = new Position(testregion2, 1 ,1);
+        assertNotEquals(tp5, tp6);
+    }
+
+    @Test
+    void set() {
+        Position tp1 = new Position(testregion1, 1 ,1);
+        Position tp2 = new Position(testregion2, 3 ,2);
+        assertNotEquals(tp1, tp2);
+        tp2.set(tp1);
+        assertEquals(tp1, tp2);
+    }
+
+    @Test
+    void setRegion() {
+        Position tp1 = new Position(testregion1, 1 ,1);
+        Position tp2 = new Position(testregion2, 1 ,1);
+        assertNotEquals(tp1, tp2);
+        tp2.setRegion(testregion1);
+        assertEquals(tp1, tp2);
+    }
+
+    @Test
+    void getRegion() {
+        Position tp1 = new Position(testregion1, 1 ,1);
+        Region compregion = tp1.getRegion();
+        assertEquals(testregion1, compregion);
+    }
+
+    @Test
+    void setX() {
+        Position tp1 = new Position(testregion1, 1 ,1);
+        Position tp2 = new Position(testregion1, 2 ,1);
+        assertNotEquals(tp1, tp2);
+        tp2.setX(1);
+        assertEquals(tp1, tp2);
+    }
+
+    @Test
+    void getX() {
+        Position tp1 = new Position(testregion1, 1 ,1);
+        int x = tp1.getX();
+        assertEquals(1, x);
+    }
+
+    @Test
+    void setY() {
+        Position tp1 = new Position(testregion1, 1 ,1);
+        Position tp2 = new Position(testregion1, 1 ,2);
+        assertNotEquals(tp1, tp2);
+        tp2.setY(1);
+        assertEquals(tp1, tp2);
+    }
+
+    @Test
+    void getY() {
+        Position tp1 = new Position(testregion1, 1 ,1);
+        int y = tp1.getY();
+        assertEquals(1, y);
+    }
+
+    @Test
+    void setXY() {
+        Position tp1 = new Position(testregion1, 1 ,1);
+        Position tp2 = new Position(testregion1, 2 ,2);
+        assertNotEquals(tp1, tp2);
+        tp2.setXY(1, 1);
+        assertEquals(tp1, tp2);
+    }
+}
