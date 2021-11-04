aaedbcbc87776fc6dd0ea123653df1a4b7bdee59
dylanzinsley <djzinsley@gmail.com>
2021-11-04 17:34:05 -0400

    Better implementation of Region

--- src/Region.java
+++ src/Region.java
@@ -1,7 +1,30 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Region {
     String nodeName;
-    ArrayList<String> subSectionNames = new ArrayList<String>();
-    String matrixName;
+    ArrayList<ArrayList<Tile>> regionGrid = new ArrayList<ArrayList<Tile>>();
+
+    //make region later
+    public Region(){
+
+    }
+    //directly pass in an array
+    //tech allows for rows to be different lengths(idk if we want this)
+    public Region(Tile[][] inputR){
+        ArrayList<Tile> buffRow = new ArrayList<Tile>();
+        for(int i = 0; i < inputR.length; i ++){
+            buffRow.clear();
+            for(int j = 0; j < inputR[i].length; j++){
+                buffRow.add(inputR[i][j]);
+            }
+            this.regionGrid.add(buffRow);
+        }
+    }
+
+    //take input from a file to create region
+    public Region(String fName){
+
+    }
+
 }
