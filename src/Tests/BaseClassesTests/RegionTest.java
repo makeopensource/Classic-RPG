d435e7b1485265d08522f2b9d2eecb7434b9d205
adam <adam38999@gmail.com>
2021-11-18 17:03:33 -0500

    Added getTile by position, and getName as well as name initialization

--- src/Tests/BaseClassesTests/RegionTest.java
+++ src/Tests/BaseClassesTests/RegionTest.java
@@ -1,6 +1,7 @@
 package Tests.BaseClassesTests;
 import BaseClasses.Tile;
 import BaseClasses.Region;
+import BaseClasses.Position;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegionTest {
@@ -13,10 +14,13 @@ class RegionTest {
                 tileMatrix[i][j] = new Tile(j,i);
             }
         }
-        Region testReg = new Region(tileMatrix);
-
+        Region testReg = new Region(tileMatrix, "testRegion");
+        Position testPos = new Position(testReg, 2, 2);
         assertEquals(2, testReg.getTile(2,3).getPositionX());
         assertEquals(3, testReg.getTile(2,3).getPositionY());
+        assertEquals("testRegion", testReg.getName());
+        assertEquals(2, testReg.getTile(testPos).getPositionX());
+        assertEquals(2, testReg.getTile(testPos).getPositionY());
         //here we need to compare the actual contents of the file so this assert is invalid.
 
     }
