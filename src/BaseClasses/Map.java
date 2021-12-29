7c088a3558b5d7d37dc3bc78fb50f422f7114e4b
jakesavi <jakesavi@buffalo.edu>
2021-12-29 15:34:32 -0500

    Added tests for Map into RegionTest.java

--- src/BaseClasses/Map.java
+++ src/BaseClasses/Map.java
@@ -1,8 +1,8 @@
 package BaseClasses;
 import java.util.*;
 public class Map {
-    Hashtable<Region,List<Region>> RegionDictionaryLocal;
-
+    Hashtable<Region, List<Region>> RegionDictionaryLocal;
+    private List<Region> emptyList = new ArrayList<>();
     public Map(Hashtable<Region,List<Region>> RegionDictionary){
         // Takes (RegionName -> ConnectedRegionList) Exclude self(RegionName) in ConnectedRegion
         RegionDictionaryLocal = RegionDictionary;
@@ -12,9 +12,19 @@ public class Map {
      *
      * @param RegionPrime The Region You are adding to the Map
      * @param ListOfConnectedRegions The List of Regions it is connected to.
+     *
      */
-    public void AddRegion(Region RegionPrime, List<Region> ListOfConnectedRegions){
+    public void addRegion(Region RegionPrime, List<Region> ListOfConnectedRegions){
         this.RegionDictionaryLocal.put(RegionPrime,ListOfConnectedRegions);
+    }
+
+    /**
+     * Overloaded version of above add Region, Except if the user does not wish to add a list of connected
+     * regions, they can do that through the use of the function addConnnectedRegion
+     * @param RegionPrime
+     */
+    public void addRegion(Region RegionPrime){
+        this.addRegion(RegionPrime,this.emptyList);
     }
 
     /**
@@ -23,7 +33,7 @@ public class Map {
      *                   If the specified region is not found. Return null
      * @return List<String> Of Connected Regions.
      */
-    public List<Region> GetListOfConnectedRegion(Region RegionName){
+    public List<Region> getListOfConnectedRegion(Region RegionName){
         if(this.RegionDictionaryLocal.contains(RegionName)){
             return this.RegionDictionaryLocal.get(RegionName);
         }
@@ -37,4 +47,30 @@ public class Map {
     public Hashtable<Region, List<Region>> getRegionDictionary() {
         return this.RegionDictionaryLocal;
     }
+
+    /**
+     *
+     * @param RegionName - Takes in the name of a specific region that you wish to connect a region to.
+     * @param RegionToAdd - Takes the name of the region you wish to connect to RegionName
+     */
+    public void addConnectedRegion(Region RegionName, Region RegionToAdd){
+        if(this.RegionDictionaryLocal.contains(RegionName)){
+               this.RegionDictionaryLocal.get(RegionName).add(RegionToAdd);
+        }
+    }
+
+    /**
+     *
+     * @param RegionName - A Region Object.
+     * @return True if the HashTable contains the Value
+     *
+     */
+    public boolean contains(Region RegionName){
+        return this.RegionDictionaryLocal.contains(RegionName);
+    }
+    /**
+     * Programmers Note: I am realizing now after making all of this i could have simply used inheritance from a HashTable
+     * and simply overloaded the values. This would have made programming this a lot more simpler. If anyone would like to fix this.
+     * Please be my guest.
+     */
 }
--- src/Tests/BaseClassesTests/RegionTest.java
+++ src/Tests/BaseClassesTests/RegionTest.java
@@ -1,7 +1,11 @@
 package Tests.BaseClassesTests;
+import BaseClasses.Map;
 import BaseClasses.Tile;
 import BaseClasses.Region;
 import BaseClasses.Position;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegionTest {
@@ -15,6 +19,7 @@ class RegionTest {
                 tileMatrix[i][j] = new Tile(testReg, j,i);
             }
         }
+
         Position testPos = new Position(testReg, 2, 2);
         assertEquals(2, testReg.getTile(2,3).getPositionX());
         assertEquals(3, testReg.getTile(2,3).getPositionY());
@@ -22,7 +27,25 @@ class RegionTest {
         assertEquals(2, testReg.getTile(testPos).getPositionX());
         assertEquals(2, testReg.getTile(testPos).getPositionY());
         //here we need to compare the actual contents of the file so this assert is invalid.
-
+        /**
+         * Anything below this is tests for a Map Object
+         */
+        Region testReg2 = new Region(tileMatrix, "testRegion2");
+        Region testReg3 = new Region(tileMatrix, "testRegion3");
+        Hashtable<Region,List<Region>> testMapHash = new Hashtable<>();
+        Map TestMap = new Map(testMapHash);
+        TestMap.addRegion(testReg);
+        TestMap.addRegion(testReg2);
+        TestMap.addRegion(testReg3);
+        assertTrue(TestMap.contains(testReg2));
+        assertTrue(TestMap.contains(testReg));
+        assertTrue(TestMap.contains(testReg3));
+        TestMap.addConnectedRegion(testReg,testReg2);
+        TestMap.addConnectedRegion(testReg,testReg3);
+        List<Region>Comparator = new ArrayList<>(){{add(testReg2);add(testReg3);}};
+        for(int i = 0; i< TestMap.getListOfConnectedRegion(testReg).size();i ++){
+            assertEquals(TestMap.getListOfConnectedRegion(testReg).get(i),Comparator.get(i));
+        }
     }
 
 
