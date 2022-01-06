17631a6a46e60912633d138a3780c43f0b0f437f
jakesavi <jakesavi@gmail.com>
2022-01-06 13:11:07 -0500

    Added a constructor to Mobile

--- src/Entities/Mobiles/Mobile.java
+++ src/Entities/Mobiles/Mobile.java
@@ -1,4 +1,18 @@
 package Entities.Mobiles;
 
+import BaseClasses.Position;
+
+import java.util.*;
+
 public class Mobile{
+    private final List<Position> pathingQueue;
+    public Mobile(List<Position> Pathing){
+        this.pathingQueue = Pathing;
+    }
+    /**
+     * Default constructor for Mobile.
+     */
+    public Mobile(){
+        this.pathingQueue = Collections.emptyList();
+    }
 }
