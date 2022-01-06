b22474b19ece3c37f4cb4ba3ba8ea5f889eb17b4
jakesavi <jakesavi@gmail.com>
2022-01-06 14:10:20 -0500

    Added Documentation to Mobile constuctors.

--- src/Entities/Mobiles/Mobile.java
+++ src/Entities/Mobiles/Mobile.java
@@ -6,11 +6,17 @@ import java.util.*;
 
 public class Mobile{
     private final List<Position> pathingQueue;
+
+    /**
+     * Constructor for Mobile, that takes a list of Position that acts as a queue for where the enemy wishes to travel,
+     * these position will be traced using a BFS method.
+     * @param Pathing - The list of Positions that will be traveled in a queue like system.
+     */
     public Mobile(List<Position> Pathing){
         this.pathingQueue = Pathing;
     }
     /**
-     * Default constructor for Mobile.
+     * Default constructor for Mobile. Will make an empty list to a pathingQueue.
      */
     public Mobile(){
         this.pathingQueue = Collections.emptyList();
