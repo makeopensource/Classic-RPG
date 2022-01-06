5f5d609049f8916e649e45b04a8dce19c3cbb005
jakesavi <jakesavi@gmail.com>
2022-01-06 14:59:08 -0500

    Removed a confusing comment.

--- src/Tests/BaseClassesTests/MobileTest.java
+++ src/Tests/BaseClassesTests/MobileTest.java
@@ -13,7 +13,6 @@ public class MobileTest {
         //Programmers Note: Mobile should also take an entity that has the ability to move.
         // This has not been developed yet.
         // TODO Make Mobile take an entity in its constructor.
-        // Unless mobile is an entity itself?
         Mobile TestMobile = new Mobile();
         assertEquals(TestMobile.pathingQueue, Collections.emptyList());
         TestMobile.add(new Position(new Region(),0,0));
